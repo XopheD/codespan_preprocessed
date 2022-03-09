fd036bc07266269a63010ed78d2e3b685d2df61f
Xophe <christophe.dousson@orange.com>
2022-03-09 15:12:09 +0100

    improve reporting features

--- src/reporting.rs
+++ src/reporting.rs
@@ -1,13 +1,16 @@
 use std::ops::Range;
 use codespan_reporting::diagnostic::{Diagnostic, Label};
-use codespan_reporting::files::Files;
+use codespan_reporting::files::{Error, Files};
+use codespan_reporting::term;
+use codespan_reporting::term::Config;
+use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
 use crate::PreprocessedFile;
 
 pub trait Report<'a,Source>
     where Source: 'a+AsRef<str>
 {
     type Position;
-    fn emit(&self, diag: &Diagnostic<<PreprocessedFile<Source> as Files<'a>>::FileId>);
+    fn emit(&self, diag: &Diagnostic<<PreprocessedFile<Source> as Files<'a>>::FileId>) -> Result<(), Error>;
     fn primary_label(&'a self, loc: impl Into<Range<Self::Position>>) -> Label<<PreprocessedFile<Source> as Files<'a>>::FileId>;
     fn secondary_label(&'a self, loc: impl Into<Range<Self::Position>>) -> Label<<PreprocessedFile<Source> as Files<'a>>::FileId>;
 }
@@ -15,12 +18,70 @@ pub trait Report<'a,Source>
 
 pub trait Reportable<'a,S:'a+AsRef<str>>
 {
-    fn emit<R:Report<'a,S>>(&self, reporting: &R);
+    fn emit<R:Report<'a,S>>(&self, reporting: &R) -> Result<(), Error>;
 }
 
 impl<'a,S:'a+AsRef<str>> Reportable<'a,S> for Diagnostic<<PreprocessedFile<S> as Files<'a>>::FileId>
 {
-    fn emit<R:Report<'a,S>>(&self, reporting: &R) {
-        reporting.emit(self)
+    fn emit<R:Report<'a,S>>(&self, reporting: &R) -> Result<(), Error> { reporting.emit(self) }
+}
+
+pub trait Locator {
+    type Position;
+    fn locate(&self, pos: Self::Position) -> usize;
+}
+
+
+pub struct PreprocessedReport<'a,S>
+    where S:'a+AsRef<str>
+{
+    writer: StandardStream,
+    config: Config,
+    source: &'a PreprocessedFile<S>
+}
+
+impl<'a,S> PreprocessedReport<'a,S>
+    where S: 'a+AsRef<str>
+{
+    pub fn new(source: &'a PreprocessedFile<S>) -> Self
+    {
+        let writer = StandardStream::stderr(ColorChoice::Always);
+        let config = codespan_reporting::term::Config::default();
+        Self { writer, config, source }
+    }
+
+    pub fn with_config(source: &'a PreprocessedFile<S>, config: Config) -> Self
+    {
+        let writer = StandardStream::stderr(ColorChoice::Always);
+        Self { writer, config, source }
+    }
+}
+
+impl<'a,S,P> Report<'a,S> for PreprocessedReport<'a,S>
+    where
+        S: 'a+AsRef<str>,
+        Self: Locator<Position=P>
+{
+    type Position = P;
+
+    fn emit(&self, diag: &Diagnostic<<PreprocessedFile<S> as Files<'a>>::FileId>) -> Result<(), Error>
+    {
+        term::emit(&mut self.writer.lock(), &self.config, self.source, &diag)
+    }
+
+    fn primary_label(&'a self, loc: impl Into<Range<Self::Position>>) -> Label<<PreprocessedFile<S> as Files<'a>>::FileId>
+    {
+        let loc = loc.into();
+        let start = self.locate(loc.start);
+        let end = self.locate(loc.end);
+        self.source.primary_label(start..end)
+    }
+
+    fn secondary_label(&'a self, loc: impl Into<Range<Self::Position>>) -> Label<<PreprocessedFile<S> as Files<'a>>::FileId>
+    {
+        let loc = loc.into();
+        let start = self.locate(loc.start);
+        let end = self.locate(loc.end);
+        self.source.secondary_label(start..end)
     }
 }
